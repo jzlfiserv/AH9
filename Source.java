dbb422eb3fd9f21b786384f98df26bf12cfdf7e6
Li <jason.li@fiserv.com>
2023-11-06 14:08:18 -0500

    final

--- Source.java
+++ Source.java
@@ -10,8 +10,16 @@ class Node<T> {
   
   class Source {
     public static <T> Node<T> reverseList(Node<T> head) {
-      // todo
-      return head;
+      Node<T> current = head;
+      Node<T> prev = null;
+      Node<T> temp;
+      while (current != null){
+        temp = current.next;
+        current.next = prev;
+        prev = current;
+        current = temp;
+      }
+      return prev;
     }
     
     public static void main(String[] args) {
@@ -28,6 +36,30 @@ class Node<T> {
         while (head != null) {
             System.out.println(head.val);
             head = head.next;            
+        }
+
+        Node<String> a = new Node<>("a");
+        Node<String> b = new Node<>("b");
+        Node<String> c = new Node<>("c");
+        Node<String> d = new Node<>("d");
+        Node<String> e = new Node<>("e");
+        Node<String> f = new Node<>("f");
+
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        e.next = f;
+        // a -> b -> c -> d -> e -> f
+
+        reverseList(a); // f -> e -> d -> c -> b -> a
+
+        Node<String> head1 = Source.reverseList(a);
+
+
+        while (head1 != null) {
+            System.out.println(head1.val);
+            head1 = head1.next;            
         }
     }
   }
